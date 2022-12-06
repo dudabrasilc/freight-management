513595ca26affbfc96292a710d6160e97cf8b2b3
Maria Coitinho <brasildu@gmail.com>
2022-12-06 03:10:21 -0600

    Add return statement with className for the form styles, form headers, form inputs and submit button

--- freight-company-client/src/components/AddTruck.js
+++ freight-company-client/src/components/AddTruck.js
@@ -42,4 +42,23 @@ function AddTruck({ addTruck }) {
       setCheckTruck("To check it, navigate to the Trucks tab.")
     })  
   }
+
+  return (
+    <div className="form-style-8">
+      <h2>Add a new truck</h2>
+      <h3>Fill out the form below to add a new truck</h3>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="truck_model" placeholder="Truck Model" value={formData.truck_model} onChange={handleChange}/>
+        <input type="number" name="truck_color" placeholder="Truck Color" value={formData.truck_color} onChange={handleChange}/>
+        <input type="number" name="license_plate" placeholder="License Plate" value={formData.license_plate} onChange={handleChange}/>
+        <input type="number" name="mileage" placeholder="Mileage" value={formData.mileage} onChange={handleChange}/>
+        <input type="text" name="vin_number" 
+        placeholder="Vin Number" value={formData.vin_number} onChange={handleChange}/>
+        <br></br>
+      <button id="button" type="submit">Submit</button>
+      <p className="submitted">{truckSubmitted}</p>
+      <p className="submitted">{checkTruck}</p>
+      </form>
+    </div>
+  )
 }
