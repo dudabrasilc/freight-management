5cc2b099829ea1cc40818af285894376764c99af
Maria Coitinho <brasildu@gmail.com>
2022-12-06 02:50:24 -0600

    Create function component AddLoad that takes addLoad function reference as props that will be used in the component to update the frontend. Add state variables along with its setter functions to handle changes to the controlled form

--- freight-company-client/src/components/AddLoad.js
+++ freight-company-client/src/components/AddLoad.js
@@ -1,1 +1,16 @@
 import { useState } from "react";
+
+function AddLoad({ addLoad }) {
+  const [formData, setFormData] = useState({
+    pickup_address: "",
+    dropoff_address: "",
+    pickup_date: "",
+    dropoff_date: "",
+    pickup_time: "",
+    dropoff_time: "",
+    weight: "",
+    rate: ""
+  })
+  const [loadSubmitted, setLoadSubmitted] = useState("")
+  const [checkLoad, setCheckLoad] = useState("")
+}
