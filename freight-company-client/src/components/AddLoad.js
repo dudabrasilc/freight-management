41571535f92b62ad53e5bd60fbf65bce8c27285f
Maria Coitinho <brasildu@gmail.com>
2022-12-06 03:04:15 -0600

    Add handle submit and handle change functions to the form and form inputs repectively

--- freight-company-client/src/components/AddLoad.js
+++ freight-company-client/src/components/AddLoad.js
@@ -13,4 +13,39 @@ function AddLoad({ addLoad }) {
   })
   const [loadSubmitted, setLoadSubmitted] = useState("")
   const [checkLoad, setCheckLoad] = useState("")
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetch("http://localhost:9292/Loads", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(resp => resp.json())
+    .then(newLoad => {
+      console.log(newLoad)
+      setFormData({
+        pickup_address: "",
+        dropoff_address: "",
+        pickup_date: "",
+        dropoff_date: "",
+        pickup_time: "",
+        dropoff_time: "",
+        weight: "",
+        rate: ""
+      })
+      addLoad(newLoad)
+      setLoadSubmitted("The load was added to the system.")
+      setCheckLoad("To check it, navigate to the Loads tab.")
+    })  
+  }
 }
