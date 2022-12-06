a04b05dc745939dbf9d71c1056a867305dad7af2
Maria Coitinho <brasildu@gmail.com>
2022-12-06 01:54:39 -0600

    Add useEffect hook to make the App component rerender and grab data from the server that will be created, in port :9292

--- freight-company-client/src/components/App.js
+++ freight-company-client/src/components/App.js
@@ -1,4 +1,35 @@
 import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import './index.css';
-import './App.css';
+
+
+function App() {
+
+  useEffect(() => {
+    fetch(`http://localhost:9292/Drivers`)
+    .then(r => r.json())
+    .then(driversData => {
+      setDrivers(driversData)
+      console.log(driversData)
+    })
+  }, [])
+
+  useEffect(() => {
+    fetch(`http://localhost:9292/Loads`)
+    .then(r => r.json())
+    .then(loadsData => {
+      setLoads(loadsData)
+      console.log(loadsData)
+    })
+  }, [])
+
+  useEffect(() => {
+    fetch(`http://localhost:9292/Trucks`)
+    .then(r => r.json())
+    .then(trucksData => {
+      setTrucks(trucksData)
+      console.log(trucksData)
+    })
+  }, [])
+
+}
