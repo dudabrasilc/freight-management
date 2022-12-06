73be4722ef413e2e075513217b9e195f41f42180
Maria Coitinho <brasildu@gmail.com>
2022-12-06 02:38:42 -0600

    Create function to handle the changes of the user input on the form, create handlesubmit function to handle the form data submission, persist it to the database and in the fronted

--- freight-company-client/src/components/AddDriver.js
+++ freight-company-client/src/components/AddDriver.js
@@ -12,4 +12,37 @@ function AddDriver({ addDriver }) {
     dl_class: ""
   })
 
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetch("http://localhost:9292/Drivers", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(resp => resp.json())
+    .then(newDriver => {
+      console.log(newDriver)
+      setFormData({
+        name: "",
+        age: "",
+        years_of_experience: "",
+        accidents: "",
+        img_url: "",
+        dl_class: ""
+      })
+      addDriver(newDriver)
+      setSubmitted("The driver was added to the system.")
+      setCheckDriver("To check it, navigate to the Drivers tab.")
+    })  
+  }
+
 }
