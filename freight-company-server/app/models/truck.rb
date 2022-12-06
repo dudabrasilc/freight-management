9c470d47b50eea57273330117f274638cf972ea1
Maria Coitinho <brasildu@gmail.com>
2022-12-06 03:41:22 -0600

    Create method to retrieve all loads from a truck

--- freight-company-server/app/models/truck.rb
+++ freight-company-server/app/models/truck.rb
@@ -2,4 +2,11 @@ class Truck < ActiveRecord::Base
   has_many :loads
   has_many :drivers, through: :loads
 
+  def truck_loads
+    Load.all.filter do |load|
+      load.truck_id == self.id
+    end
+  end
+
+
 end
