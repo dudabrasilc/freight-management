adf635ec02708e4708aa39a90395ec26f60c16d2
Maria Coitinho <brasildu@gmail.com>
2022-12-06 03:05:33 -0600

    Add return statement to the AddLoad component with a className to the form styles, add headers, form tag with inputs and for submit button

--- freight-company-client/src/components/AddLoad.js
+++ freight-company-client/src/components/AddLoad.js
@@ -48,4 +48,26 @@ function AddLoad({ addLoad }) {
       setCheckLoad("To check it, navigate to the Loads tab.")
     })  
   }
+  
+  return (
+    <div className="form-style-8">
+      <h2>Add a new load</h2>
+      <h3>Fill out the form below to add a new load</h3>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="pickup_address" placeholder="Pickup Address" value={formData.pickup_address} onChange={handleChange}/>
+        <input type="text" name="dropoff_address" placeholder="Dropoff Address" value={formData.dropoff_address} onChange={handleChange}/>
+        <input type="text" name="pickup_date" placeholder="Pickup Date (mm/dd/yyyy)" value={formData.pickup_date} onChange={handleChange}/>
+        <input type="text" name="dropoff_date" placeholder="Dropoff Date (mm/dd/yyyy)" value={formData.dropoff_date} onChange={handleChange}/>
+        <input type="text" name="pickup_time" placeholder="Pickup Time" value={formData.pickup_time} onChange={handleChange}/>
+        <input type="text" name="dropoff_time" placeholder="Dropoff Time" value={formData.dropoff_time} onChange={handleChange}/>
+        <input type="text" name="weight" placeholder="Weight" value={formData.weight} onChange={handleChange}/>
+        <input type="text" name="rate" placeholder="Rate" value={formData.rate} onChange={handleChange}/>
+        <br></br>
+        <button id="button" type="submit">Submit</button>
+        <br></br>
+        <p className="submitted">{loadSubmitted}</p>
+        <p className="submitted">{checkLoad}</p>
+      </form>
+    </div>
+  )
 }
