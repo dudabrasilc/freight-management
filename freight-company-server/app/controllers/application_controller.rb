95340200266babbff958fbcc324e1a96acca856a
Maria Coitinho <brasildu@gmail.com>
2022-12-06 03:26:31 -0600

    Create two get requests to be handled once the user requests with the specified path to the server: /Drivers path retrieves all drivers data from database; /Drivers/:id retrieves driver based on its id

--- freight-company-server/app/controllers/application_controller.rb
+++ freight-company-server/app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+  #READ DRIVER MODEL
+
+  get "/Drivers" do
+    drivers = Driver.all
+    drivers.to_json
+  end
+
+  get "/Drivers/:id" do
+    driver = Driver.find(params[:id])
+    driver.to_json
+  end
+
+
 end
