bb9ca6c321c8a1bac83eb16f1531df41134921bf
Maria Coitinho <brasildu@gmail.com>
2022-12-07 01:27:17 -0600

    Create handler function to remove driver and update the dom with a new list of drivers without the removed one

--- freight-company-client/src/components/DriversList.js
+++ freight-company-client/src/components/DriversList.js
@@ -2,4 +2,10 @@ import { Link } from 'react-router-dom';
 import DriverCard from "./DriverCard";
 
 function DriversList({ drivers, setDrivers }) {
+
+  function handleRemoveDriver(removedDriver) {
+    const updatedDriversList = drivers.filter((driver) => driver.id !== removedDriver.id);
+    setDrivers(updatedDriversList);
+  }
+  
 }
