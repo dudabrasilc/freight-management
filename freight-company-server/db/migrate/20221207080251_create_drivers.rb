7136579736abe87a38a69a21ce6276f484ca4a22
Maria Coitinho <brasildu@gmail.com>
2022-12-07 02:12:09 -0600

    Create drivers table with its attributes and data types

--- freight-company-server/db/migrate/20221207080251_create_drivers.rb
+++ freight-company-server/db/migrate/20221207080251_create_drivers.rb
@@ -1,4 +1,14 @@
 class CreateDrivers < ActiveRecord::Migration[6.1]
   def change
+    create_table :drivers do |t|
+      t.string :name
+      t.integer :age
+      t.integer :years_of_experience
+      t.integer :accidents
+      t.string :dl_class
+      t.string :img_url
+      t.boolean :passed_drug_test
+      t.boolean :clean_driving_record
+    end
   end
 end
