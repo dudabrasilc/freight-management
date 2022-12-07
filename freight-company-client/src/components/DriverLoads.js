b1375afb46235258bd5b653b73bc3dc1290995d3
Maria Coitinho <brasildu@gmail.com>
2022-12-06 19:06:24 -0600

    Create variable that returns XML tags for displaying a driver load information, maped out from driverLoads

--- freight-company-client/src/components/DriverLoads.js
+++ freight-company-client/src/components/DriverLoads.js
@@ -1,3 +1,19 @@
 function DriverLoads({ driverLoads }) {
   console.log(driverLoads)
+
+  const loadsList = driverLoads.map(load => {
+    return (
+        <li key={load.id}>
+          <h3>Load Number #{load.id}</h3>
+          <p> ‣ Pickup Address: {load.pickup_address}</p>
+          <p> ‣ Dropoff Address: {load.dropoff_address}</p>
+          <p> ‣ Pickup Date: {load.pickup_date}</p>
+          <p> ‣ Dropoff Date: {load.dropoff_date}</p>
+          <p> ‣ Pickup Time: {load.pickup_time}</p>
+          <p> ‣ Dropoff Time: {load.dropoff_time}</p>
+          <p> ‣ Weight: {load.weight}</p>
+          <p> ‣ Rate: {load.rate}</p>     
+        </li>  
+    )
+  })
 }
