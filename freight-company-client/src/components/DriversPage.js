946575f31017afd07ad132dd5b64c28700cd59d3
Maria Coitinho <brasildu@gmail.com>
2022-12-07 01:37:04 -0600

    Import react router hooks and function components

---
+++ freight-company-client/src/components/DriversPage.js
@@ -0,0 +1,4 @@
+import { Route, useRouteMatch, Switch } from "react-router-dom";
+import DriversList from "./DriversList.js";
+import DisplayDriver from "./DisplayDriver.js";
+import DriverLoads from "./DriverLoads.js";
