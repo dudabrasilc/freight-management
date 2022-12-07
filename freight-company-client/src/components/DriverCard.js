33bbecaf48ba9f4a0a254159cf5aef7dfc9489ad
Maria Coitinho <brasildu@gmail.com>
2022-12-06 19:03:41 -0600

    Add return statement of contents for a driver card, such as image, name and the delete button to remove a driver

--- freight-company-client/src/components/DriverCard.js
+++ freight-company-client/src/components/DriverCard.js
@@ -10,4 +10,12 @@ function DriverCard({ name, img_url, id, onRemoveDriver }) {
     });
   }
 
+  return (
+    <div className="card">
+      <img src={img_url} alt={"driver picture"} />
+      <h3 className="driver-name">{name}</h3>
+      <button className="card-btn delete" onClick={handleDelete}>Remove</button>
+    </div>
+  )
+
 }
