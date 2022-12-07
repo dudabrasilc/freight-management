fd6b2319c098558ad9d2c015683ca7e246662909
Maria Coitinho <brasildu@gmail.com>
2022-12-06 18:58:38 -0600

    Add return statement with all html content information about the requested driver, including css styles, dynamic input to display and hide contetnt, button to handle clicks and persist information to server

--- freight-company-client/src/components/DisplayDriver.js
+++ freight-company-client/src/components/DisplayDriver.js
@@ -92,4 +92,31 @@ function DisplayDriver({ drivers, onUpdateDriver }) {
       </>
     )
   }
+  else {
+    return (
+      <div className="display">
+        <img className="image-display" src={matched[0].img_url} alt="image" />
+        <h2>{matched[0].name}</h2>
+        <p>Age: {matched[0].age}</p>
+        <p>Years of Experience: {matched[0].years_of_experience}</p>
+        <p>Accidents: {matched[0].accidents}</p>
+        <p>Driver's License Class: {matched[0].dl_class}</p>
+        <hr></hr>
+        <div>
+          <input type="submit" className="" value={showResults ? "Hide Loads" : "Display loads" } onClick={handleShow} />
+          { showResults ? <DriverLoads driverLoads={driverLoads}/> : null }
+        </div>
+        <hr></hr>
+        <div>
+          <input type="submit" className="" value={showTrucks ? "Hide Trucks" : "Display Trucks" } onClick={handleShowTrucks} />
+          { showTrucks ? <DriverTrucks driverTrucks={driverTrucks}/> : null }
+        </div>
+        <hr></hr>
+        <p> Check the box below if the driver passed on the drug test.</p>
+        <button className={drugTestPassed ? "selected" : ""} onClick={handleDrugTest}>{drugTestPassed ? "☑️" : "☐"}</button>
+        <p> Check the box below if the driver has a clean driving record.</p>
+        <button className={drivingRecord ? "selected" : ""} onClick={handleDrivingRecord}>{drivingRecord ? "☑️" : "☐"}</button>
+      </div>
+    )
+  }
 }
