6951c2d74de3a59aed1d4690b831f98e37662de8
Maria Coitinho <brasildu@gmail.com>
2022-12-06 18:43:16 -0600

    Import router hook, state hooks and function component files

---
+++ freight-company-client/src/components/DisplayDriver.js
@@ -0,0 +1,5 @@
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useRouteMatch } from "react-router-dom";
+import DriverLoads from './DriverLoads';
+import DriverTrucks from './DriverTrucks';
