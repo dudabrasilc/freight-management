fe78488966ac562af5cdfc0f3b1c9bfb53a41033
Maria Coitinho <brasildu@gmail.com>
2022-12-07 02:11:35 -0600

    Create loads table with specific attributes

--- freight-company-server/db/migrate/20221207080522_create_loads.rb
+++ freight-company-server/db/migrate/20221207080522_create_loads.rb
@@ -1,4 +1,14 @@
 class CreateLoads < ActiveRecord::Migration[6.1]
   def change
+    create_table :loads do |t|
+      t.string :pickup_address
+      t.string :dropoff_address
+      t.string :pickup_date
+      t.string :dropoff_date
+      t.string :pickup_time
+      t.string :dropoff_time
+      t.string :weight
+      t.string :rate
+    end
   end
 end
