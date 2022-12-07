047cdf4f93be946aa94f9514473e40faabc38237
Maria Coitinho <brasildu@gmail.com>
2022-12-06 19:02:38 -0600

    Create handleDelete handler function to handle a driver delete action and persist on both client and server sides

--- freight-company-client/src/components/DriverCard.js
+++ freight-company-client/src/components/DriverCard.js
@@ -1,3 +1,13 @@
 function DriverCard({ name, img_url, id, onRemoveDriver }) {
 
+  function handleDelete() {
+    fetch(`http://localhost:9292/Drivers/${id}`, {
+    method: "DELETE",
+  })
+    .then((r) => r.json())
+    .then((driver) => {
+      onRemoveDriver(driver)
+    });
+  }
+
 }
