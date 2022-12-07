542e474cd85564bc0efa8a302032a151a7524f2c
Maria Coitinho <brasildu@gmail.com>
2022-12-06 18:46:49 -0600

    Add state variables to retrieve data for driver loads and trucks. Add state variables to update driver information dinamically

--- freight-company-client/src/components/DisplayDriver.js
+++ freight-company-client/src/components/DisplayDriver.js
@@ -1,5 +1,13 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useRouteMatch } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { useRouteMatch } from "react-router-dom";
 import DriverLoads from './DriverLoads';
 import DriverTrucks from './DriverTrucks';
+
+function DisplayDriver({ drivers, onUpdateDriver }) {
+  const [drugTestPassed, setDrugTestPassed] = useState(false)
+  const [drivingRecord, setDrivingRecord] = useState(false)
+  const [driverLoads, setDriverLoads] = useState([])
+  const [driverTrucks, setDriverTrucks] = useState([])
+  const [showResults, setShowResults] = useState(false)
+  const [showTrucks, setShowTrucks] = useState(false)
+}
