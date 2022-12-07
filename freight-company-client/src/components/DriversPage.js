96c51af951c59d373ee9b75fbcccf81b3b629451
Maria Coitinho <brasildu@gmail.com>
2022-12-07 01:39:01 -0600

    Create DriversPage component that takes props of drivers data, setDrivers setter function, updateDriver function. Store current route in the match variable, return a switch component with nested route.

--- freight-company-client/src/components/DriversPage.js
+++ freight-company-client/src/components/DriversPage.js
@@ -2,3 +2,15 @@ import { Route, useRouteMatch, Switch } from "react-router-dom";
 import DriversList from "./DriversList.js";
 import DisplayDriver from "./DisplayDriver.js";
 import DriverLoads from "./DriverLoads.js";
+
+
+function DriversPage({ drivers, setDrivers, onUpdateDriver }) {
+  const match = useRouteMatch()
+
+  return (
+    <Switch>
+      <Route exact path='/Drivers'>
+        <DriversList drivers={ drivers } setDrivers={setDrivers} />
+      </Route>
+      </Switch>
+)}
