cbbcc5a57bbf88055337392b62f86fab7a425165
Maria Coitinho <brasildu@gmail.com>
2022-12-06 18:48:14 -0600

    Create variable to hold value of the current params and filter drivers data array to match with the current params from URL

--- freight-company-client/src/components/DisplayDriver.js
+++ freight-company-client/src/components/DisplayDriver.js
@@ -4,10 +4,20 @@ import DriverLoads from './DriverLoads';
 import DriverTrucks from './DriverTrucks';
 
 function DisplayDriver({ drivers, onUpdateDriver }) {
-  const [drugTestPassed, setDrugTestPassed] = useState(false)
-  const [drivingRecord, setDrivingRecord] = useState(false)
   const [driverLoads, setDriverLoads] = useState([])
   const [driverTrucks, setDriverTrucks] = useState([])
+  const [drugTestPassed, setDrugTestPassed] = useState(false)
+  const [drivingRecord, setDrivingRecord] = useState(false)
   const [showResults, setShowResults] = useState(false)
   const [showTrucks, setShowTrucks] = useState(false)
+
+  const match = useRouteMatch()
+  console.log(match.url)
+
+  const params = useParams()
+  const matched = drivers.filter(driver => {
+    return Number(params.id) === driver.id
+  })
+
+
 }
