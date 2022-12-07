a8438b700e43127f789fee4f9c9f5f985e844687
Maria Coitinho <brasildu@gmail.com>
2022-12-07 02:10:54 -0600

    create_table in create trucks migration file, specifying truck attributes

--- freight-company-server/db/migrate/20221207080529_create_trucks.rb
+++ freight-company-server/db/migrate/20221207080529_create_trucks.rb
@@ -1,4 +1,11 @@
 class CreateTrucks < ActiveRecord::Migration[6.1]
   def change
+    create_table :trucks do |t|
+      t.string :truck_model
+      t.string :truck_color
+      t.string :license_plate
+      t.string :mileage
+      t.string :vin_number
+    end
   end
 end
