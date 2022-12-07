dc5719d5c32941c56e5b5a0c4a9b5fb18ad6685a
Maria Coitinho <brasildu@gmail.com>
2022-12-07 01:46:27 -0600

    Loop through loads data created with faker gem to generate 25 loads with its own attributes

--- freight-company-server/db/migrate/seeds.rb
+++ freight-company-server/db/migrate/seeds.rb
@@ -13,3 +13,20 @@ puts "🌱 Seeding spices..."
   )
 
 end
+
+25.times do
+
+  Load.create(
+    pickup_address: "#{Faker::Address.street_address}, zip: #{Faker::Address.zip_code}, #{Faker::Address.city}, #{Faker::Address.state}",
+    dropoff_address: "#{Faker::Address.street_address}, zip: #{Faker::Address.zip_code}, #{Faker::Address.city}, #{Faker::Address.state}",
+    pickup_date: "1/#{rand(1..15)}/2023",
+    dropoff_date: "1/#{rand(15..30)}/2023",
+    pickup_time: "#{rand(1..23)}:00",
+    dropoff_time: "#{rand(1..23)}:00",
+    weight: "#{rand(0..48000)} lbs",
+    rate: "$#{rand(1500..8000)}",
+    driver_id: rand(1..10),
+    truck_id: rand(1..15)
+  )
+
+end
